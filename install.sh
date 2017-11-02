5d5b17bfa638df49f183c931029382ed667bc3b1
Boris Kremenovic <info@boriskremenovic.com>
2017-11-02 02:11:18 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-sudo cp libs/libsgfdu03.so /usr/local/lib
-sudo cp libs/libsgfpamx.so /usr/local/lib
-sudo cp libs/libsgfplib.so /usr/local/lib
-sudo cp libs/libsgnfiq.so /usr/local/lib
-sudo cp libs/libpysgfplib.so /usr/local/lib
-sudo cp libs/libsgfdu06.so /usr/local/lib
+sudo cp $(dirname "$0")/libs/libsgfdu03.so /usr/local/lib
+sudo cp $(dirname "$0")/libs/libsgfpamx.so /usr/local/lib
+sudo cp $(dirname "$0")/libs/libsgfplib.so /usr/local/lib
+sudo cp $(dirname "$0")/libs/libsgnfiq.so /usr/local/lib
+sudo cp $(dirname "$0")/libs/libpysgfplib.so /usr/local/lib
+sudo cp $(dirname "$0")/libs/libsgfdu06.so /usr/local/lib
 
 sudo ldconfig /usr/local/lib
